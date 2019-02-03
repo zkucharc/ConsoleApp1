d5d6b781d93dcbd5fd4a489462ba80fa7e3a02f4
zkucharc <zkucharc@gmail.com>
2019-02-03 12:26:07 -0500

    Added e letter

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hellooo World!");
+            Console.WriteLine("Heellooo World!");
         }
     }
 }
