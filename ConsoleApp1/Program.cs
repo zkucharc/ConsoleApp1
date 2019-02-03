65f882a5e4144a0c7257b6f6bb3a43bdbf98b8fe
zkucharc <zkucharc@gmail.com>
2019-02-03 12:27:30 -0500

    Added l letter

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Heellooo World!");
+            Console.WriteLine("Heelllooo World!");
         }
     }
 }
